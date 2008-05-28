e1ad06969c3d3982b06ebd7da3777a0926501e7c
Matt Aimonetti <mattaimonetti@gmail.com>
2008-05-28 00:32:37 -0700

    first commit

---
+++ lib/multibyte.rb
@@ -0,0 +1,9 @@
+$:.unshift File.dirname(__FILE__)
+
+module Multibyte #:nodoc:
+  DEFAULT_NORMALIZATION_FORM = :kc
+  NORMALIZATIONS_FORMS = [:c, :kc, :d, :kd]
+  UNICODE_VERSION = '5.0.0'
+end
+
+require 'multibyte/chars'
